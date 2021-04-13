54eef01022150a8eff434b9d9a2675df8e88b3ed
Veronezegui <gui.veroneze@gmail.com>
2021-04-12 22:32:19 -0300

    atualização - 12/04/2021

--- src/components/input/styles.ts
+++ src/components/input/styles.ts
@@ -1,6 +1,15 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+import Tooltip from '../Tooltip';
+
+
+interface ContainerProps {
+    isFocused: boolean;
+    isFilled: boolean;
+    isErrored: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
 
     background: #232129;
     border-radius: 10px;
@@ -16,10 +25,29 @@ export const Container = styled.div`
         margin-top: 8px;
     }
 
+    ${(props) => props.isErrored
+        && css`
+            border-color: #c53030;
+        `}
+
+
+    ${(props) => props.isFocused
+        && css`
+            color: #ff9000;
+            border-color: #ff9000;
+        `}
+    
+    ${(props) => props.isFilled
+        && css`
+            color: #ff9000;
+        `}
+
+
     input {
         background: transparent;
         flex: 1;
         border: 0;
+        color: #f4ede8;
 
         &::placeholder {
             color: #666360;
@@ -29,5 +57,23 @@ export const Container = styled.div`
 
     svg {
         margin-right: 16px;
+    }
+`;
+
+export const Error = styled(Tooltip)`
+
+    height: 20px;
+    margin-left: 16px;
+    svg {
+        margin: 0
+    }
+
+    span {
+        background: #c53030;
+        color: #FFF;
+
+        &::before {
+            border-color: #c53030 transparent;
+        }
     }
 `;
--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -17,7 +17,7 @@ const SignIn: React.FC = () => (
                 <h1>Faça seu logon</h1>
 
                 <Input name="email" icon={FiMail} placeholder="E-mail"/>
-                <Input name="password" icon={FiLock} placeholder="Senha"/>
+                <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
                 <Button type="submit">Entrar</Button>
                 <a href="forgot">Esqueci minha senha</a>
             </form>
